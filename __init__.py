bdaeb872a6e05339e4f793415c655b6ca5bef527
Lizmarquez2 <esthef356@outlook.com>
2026-08-23 21:23:14 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,38 @@
-# views/__init__.py
+# modules/__init__.py
+
+from .data_loader import (
+    cargar_todos_datos,
+    obtener_tasas_cambio,
+    obtener_personas,
+    obtener_metas
+)
+
+from .conversiones import (
+    sol_a_cad,
+    cad_a_sol,
+    formato_moneda_soles,
+    formato_moneda_cad
+)
+
+from .calculadora import (
+    calcular_totales_movimientos,
+    calcular_progreso_bolsa,
+    calcular_por_persona,
+    obtener_kpis_principales
+)
+
+__all__ = [
+    'cargar_todos_datos',
+    'obtener_tasas_cambio',
+    'obtener_personas',
+    'obtener_metas',
+    'sol_a_cad',
+    'cad_a_sol',
+    'formato_moneda_soles',
+    'formato_moneda_cad',
+    'calcular_totales_movimientos',
+    'calcular_progreso_bolsa',
+    'calcular_por_persona',
+    'obtener_kpis_principales'
+]
 
